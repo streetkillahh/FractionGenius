30ba2e72e6305b94ee3577540f58f31e260ddf89
streetkillahh <streetkillahh@mail.ru>
2024-07-17 04:36:58 +0500

    Add: Navigation between pages& styles

---
+++ FractionGenius/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FractionGenius.Views
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Summation());
+        }
+    }
+}
--- FractionGenius/Views/Summation.xaml.cs
+++ FractionGenius/Views/Summation.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FractionGenius
+namespace FractionGenius.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для Summation.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Summation : Page
     {
-        public MainWindow()
+        public Summation()
         {
             InitializeComponent();
-            mainFrame.Content = new Views.MainPage();
         }
     }
 }
