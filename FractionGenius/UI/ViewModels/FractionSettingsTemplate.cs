ff5e3ff3231142f7734740f5ec512c15fe0deaba
streetkillahh <streetkillahh@mail.ru>
2024-08-21 16:41:04 +0500

    Add Precise adjustment feature for fraction generator

---
+++ FractionGenius/UI/ViewModels/FractionSettingsTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionGenius.UI.ViewModels
+{
+    public class FractionSettingsTemplate
+    {
+        public string Name { get; set; } // Название шаблона настроек
+        public bool IncludeBrackets { get; set; } // Включение/выключение скобок
+        public int MinValue { get; set; } // Минимальное значение чисел в дробях
+        public int MaxValue { get; set; } // Максимальное значение чисел в дробях
+        public int FractionCount { get; set; } // Количество генерируемых дробей
+        public string Operations { get; set; } // Доступные операции между дробями (+, -, *, /)
+    }
+
+}
